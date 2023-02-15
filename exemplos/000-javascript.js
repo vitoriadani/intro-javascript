756c6381c5554034450ac4451114e94ee81638e0
vitoriadani <124819172+vitoriadani@users.noreply.github.com>
2023-02-15 17:30:00 -0300

    variaveis

--- exemplos/000-javascript.js
+++ exemplos/000-javascript.js
@@ -1,1 +1,30 @@
+// o comando abaixo apresenta  uma mensagem na tela
 console.log("ola estou programando em javascript");
+/*
+       comentario que ocupa mais
+       de uma linha
+*/
+
+// criaçao de variavel do tipo texto
+let nome = 'daniele vitoria';
+// criaçao de variavel do tipo numerico (inteiro)
+let idade = 16;
+// criaçao de variavel do tipo numerico (decimal)
+let altura = 1.68;
+// criaçao de variavel do tipo verdadeiro/falso
+let trabalha = true; // = verdadeiro | false = falso
+// exemplo de variavel de nome composto
+let corFavorita = 'preto';
+// exemplo de variavel do tipo lista de valores
+let cores_favoritas = ['preto', 'azul bebe', 'roxo pastel'];
+// exemplo de variavel que armazena um objeto
+let endereco ={
+    'longradouro'        : 'rua',
+    'nome_longradouro'   : 'brasil',
+    'numero'             : '50 a',
+    'bairro'             : 'centro',
+    'cidade'             : 'tupa',
+
+};
+let covid;
+let dengue = null;
